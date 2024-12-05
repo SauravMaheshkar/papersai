610d35275200f4a3d0931a3da90aab25969a119d
Saurav Maheshkar <sauravvmaheshkar@gmail.com>
2024-12-05 23:12:39 +0000

    feat: move llm tasks to litellm+weave

--- papersai/engine/summarize.py
+++ papersai/engine/summarize.py
@@ -1,18 +1,48 @@
-from typing import List
-
-from llama_index.core import Document
-from llama_index.core.response_synthesizers import TreeSummarize
+import weave
+from litellm import completion
 
 
-def get_summary(
-    context: List[Document], verbose: bool = False, rich_metadata=None
-) -> str:
-    summarizer = TreeSummarize(verbose=verbose)
-    summary = summarizer.get_response(
-        "Summarize the paper", [doc.text for doc in context]
-    )
+__all__ = ["Summarizer"]
 
-    if rich_metadata:
-        rich_metadata[0].update(rich_metadata[1], advance=1)
 
-    return str(summary)
+# References:
+# * https://github.com/run-llama/llama_index/blob/eeb068f75ce2588636f685c5fb91ee88760ea5ce/llama-index-core/llama_index/core/prompts/default_prompts.py#L10
+SUMMARY_PROMPT: str = (
+    "Write a summary of the following. Try to use only the "
+    "information provided. "
+    "Try to include as many key details as possible.\n"
+    "\n"
+    "\n"
+    "{context_str}\n"
+    "\n"
+    "\n"
+    'SUMMARY:"""\n'
+)
+
+
+class Summarizer(weave.Model):
+    model: str
+
+    def __init__(self, model) -> None:
+        super().__init__(model=model)
+
+    @weave.op()
+    def summarize(
+        self,
+        context: str,
+        rich_metadata=None,
+    ) -> str:
+        response = completion(
+            model=self.model,
+            messages=[
+                {
+                    "role": "user",
+                    "content": SUMMARY_PROMPT.format(context_str=context),
+                }
+            ],
+        )
+
+        if rich_metadata:
+            rich_metadata[0].update(rich_metadata[1], advance=1)
+
+        return str(response.choices[0].message.content)
